d3be58736a9bb1bd42e168e4ab49102a779ba455
gunwoongPark <olympiodoros@naver.com>
2021-04-11 21:07:22 +0900

    폰트 설정, 내비게이션 바 생성

--- YoucandoEAT/src/App.js
+++ YoucandoEAT/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./App.css";
+import { createGlobalStyle } from "styled-components";
+
+import NavBar from "./components/NavBar/NavBarContainer";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family : 'NanumSquare';
+    max-width:500px;
+    background:tomato;
+  }
+`;
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
---
+++ YoucandoEAT/src/components/NavBar/NavBarPresenter.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0 1px 10px 1px rgba(0, 0, 0, 0.3);
+  background: white;
+`;
+
+const Title = styled.h1`
+  margin-left: 1rem;
+  font-size: 20px;
+`;
+
+const HamburgerBtn = styled.button`
+  margin-right: 1rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background: white;
+  border-radius: 8px;
+
+  &:active {
+    filter: brightness(85%);
+  }
+`;
+
+function NavBarPresenter() {
+  return (
+    <>
+      <Container>
+        <Title>You can do EAT</Title>
+
+        <HamburgerBtn>
+          <GiHamburgerMenu size="24" />
+        </HamburgerBtn>
+      </Container>
+    </>
+  );
+}
+
+export default NavBarPresenter;
