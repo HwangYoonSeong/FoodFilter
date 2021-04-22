7dbdf6d1660096df92890a306abc9427020b21aa
gunwoongPark <olympiodoros@naver.com>
2021-04-22 14:34:51 +0900

    FunctionalCode 삭제, 게시판 데이터 불러오는 형식으로 변경

--- YoucandoEAT/src/components/Community/CommunityContainer.js
+++ YoucandoEAT/src/components/Community/CommunityContainer.js
@@ -1,81 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CommunityPresenter from "./CommunityPresenter";
 import Dummy from "../../assets/권은비2.jpg";
 
 function CommunityContainer() {
-  const [posts, setPosts] = useState([
-    {
-      title: "Dummy Title1",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+  const [posts, setPosts] = useState([]);
 
-    {
-      title: "Dummy Title2",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+  useEffect(() => {
+    setPosts([
+      {
+        title: "Dummy Title1",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title3",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+      {
+        title: "Dummy Title2",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title4",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+      {
+        title: "Dummy Title3",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title5",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+      {
+        title: "Dummy Title4",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title6",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+      {
+        title: "Dummy Title5",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title7",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+      {
+        title: "Dummy Title6",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title8",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
+      {
+        title: "Dummy Title7",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
 
-    {
-      title: "Dummy Title9",
-      content: "dummy content",
-      date: "2021-04-18 21:52",
-      writer: "Lena",
-      thumbnail: Dummy,
-    },
-  ]);
+      {
+        title: "Dummy Title8",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
+
+      {
+        title: "Dummy Title9",
+        content: "dummy content",
+        date: "2021-04-18 21:52",
+        writer: "Lena",
+        thumbnail: Dummy,
+      },
+    ]);
+  }, []);
 
   return (
     <>
