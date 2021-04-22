e1e7c842d5400fc68dc8045924d717b047ae6a32
gunwoongPark <olympiodoros@naver.com>
2021-04-22 15:37:49 +0900

    css 수정

--- YoucandoEAT/src/components/SelectAllergy/SAPresenter.js
+++ YoucandoEAT/src/components/SelectAllergy/SAPresenter.js
@@ -1,17 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 
-const TitleContainer = styled.div`
+const Title = styled.div`
   display: flex;
   position: fixed;
   width: 100%;
   align-items: center;
-  height: 80px;
+  padding: 1rem 0 1rem 0;
   background: black;
   font-size: 20px;
   color: white;
-  margin-top: 45px;
   justify-content: space-around;
+  bottom: 0;
+`;
+
+const IngrdContainer = styled.ul`
+  vertical-align: center;
+  width: 100%;
+  margin-top: 2.5rem;
+  padding: 0;
+  margin-bottom: 5rem;
+`;
+
+const Ingrd = styled.li`
+  padding: 10px;
+  display: flex;
+  align-items: center;
+
+  background: ${(props) => (props.isclick ? "#ced4da" : "white")};
+`;
+const IngrdImg = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const IngrdName = styled.p`
+  margin-left: 20px;
+  font-size: 20px;
+  font-weight: bold;
 `;
 
 const SaveBtn = styled.button`
@@ -20,59 +46,35 @@ const SaveBtn = styled.button`
   width: 50px;
   height: 50px;
   border: none;
-  outline: none;
   &:active {
     filter: brightness(85%);
   }
 `;
 
-const Allergy = styled.ul`
-  position: absolute;
-  vertical-align: center;
-  margin-top: 125px;
-  width: 100%;
-  z-index: -1;
-`;
-
-const Item = styled.li`
-  padding: 10px;
-  display: flex;
-  align-items: center;
-  background: ${(props) => (props.isclick ? "#ced4da" : "white")};
-`;
-
-const FoodImg = styled.img`
-  width: 100px;
-  height: 100px;
-`;
-
-const FoodName = styled.p`
-  margin-left: 20px;
-  font-size: 20px;
-  font-weight: bold;
-`;
-
 function SAPresenter({ AllergyList, itemClick, save }) {
   return (
     <>
-      <TitleContainer>
-        Select from <br />
-        21 Food Allergy Trigger
+      <Title>
+        Select from <br></br>21 Food Allergy Trigger
         <SaveBtn onClick={save}>
           <b>SAVE</b>
         </SaveBtn>
-      </TitleContainer>
+      </Title>
 
-      <Allergy>
-        {AllergyList.map((el, idx) => {
+      <IngrdContainer>
+        {AllergyList.map((el, index) => {
           return (
-            <Item isclick={el.checked} key={idx} onClick={() => itemClick(idx)}>
-              <FoodImg src={el.image} alt="FoodImg" />
-              <FoodName>{el.name}</FoodName>
-            </Item>
+            <Ingrd
+              isclick={el.checked}
+              key={index}
+              onClick={() => itemClick(index)}
+            >
+              <IngrdImg src={el.image} alt="FoodImg" />
+              <IngrdName>{el.name}</IngrdName>
+            </Ingrd>
           );
         })}
-      </Allergy>
+      </IngrdContainer>
     </>
   );
 }
