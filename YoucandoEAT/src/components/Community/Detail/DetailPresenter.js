b6ff42e6d23b911cf4d1873a2da1a0171cb07f73
gunwoongPark <olympiodoros@naver.com>
2021-05-10 19:52:32 +0900

    디테일 뷰 상단 부분 구현

--- YoucandoEAT/src/components/Community/Detail/DetailPresenter.js
+++ YoucandoEAT/src/components/Community/Detail/DetailPresenter.js
@@ -2,8 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import Dummy from "../../../assets/Lenna.png";
 
-const DetailContainer = styled.div`
-  margin-top: 50px;
+const ContentsContainer = styled.div`
+  margin: 50px 0.5rem 0 0.5rem;
+  border-bottom: solid 1px #adb5bd;
 `;
 
 const UserImage = styled.img`
@@ -13,21 +14,32 @@ const UserImage = styled.img`
   border-radius: 0.5rem;
 `;
 
+const CommentsContainer = styled.div`
+  margin: 0 0.5rem 0 0.5rem;
+`;
+
 function DetailPresenter() {
   return (
-    <DetailContainer>
-      <table style={{ marginLeft: "0.5rem" }}>
-        <tr>
-          <td rowSpan="2">
-            <UserImage src={Dummy} alt="dummy image" />
-          </td>
-          <td>bung1438@gmail.com</td>
-        </tr>
-        <tr>
-          <td style={{ fontSize: "0.8rem" }}>04/18 21:52</td>
-        </tr>
-      </table>
-    </DetailContainer>
+    <>
+      <ContentsContainer>
+        <table>
+          <tr>
+            <td rowSpan="2">
+              <UserImage src={Dummy} alt="dummy image" />
+            </td>
+            <td>bung1438@gmail.com</td>
+          </tr>
+          <tr>
+            <td style={{ fontSize: "0.75rem", color: "#adb5bd" }}>
+              04/18 21:52
+            </td>
+          </tr>
+        </table>
+        <h1 style={{ fontSize: "1.25rem" }}>Dummy Title</h1>
+        <p>dummy content</p>
+      </ContentsContainer>
+      <CommentsContainer></CommentsContainer>
+    </>
   );
 }
 
