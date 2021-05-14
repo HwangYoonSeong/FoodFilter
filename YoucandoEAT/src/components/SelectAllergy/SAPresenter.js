c31ae363bad44a518f6991af82d28e35d5435580
yooncastle <dbstjd427@gmail.com>
2021-05-14 16:51:47 +0900

    Select Ingredients 디자인
    * Header와 Footer 분리
    * add 버튼 추가

--- YoucandoEAT/src/components/SelectAllergy/SAPresenter.js
+++ YoucandoEAT/src/components/SelectAllergy/SAPresenter.js
@@ -1,23 +1,39 @@
 import React from "react";
 import styled from "styled-components";
-
+import { BiAddToQueue } from "react-icons/bi";
+import { GiSaveArrow } from "react-icons/gi";
 const Title = styled.div`
   display: flex;
   position: fixed;
   width: 100%;
-  align-items: center;
-  padding: 1rem 0 1rem 0;
+  padding: 1rem 0 0 0;
   background: black;
   font-size: 20px;
   color: white;
-  justify-content: space-around;
-  bottom: 0;
+  top:0;
+  margin-top: 2.8rem;
+  justify-content:space-around;
+`;
+const Footer = styled.div`
+  display: flex;
+  position: fixed;
+  height:200px;
+  padding: 1rem 1rem 1rem 0;
+  font-size: 20px;
+  right:0;
+  bottom:0;
+  flex-direction: column;
+  justify-content:space-around;
+`;
+const SubTitle = styled.p`
+   font-size:13px;
+   text-align:center;
 `;
 
 const IngrdContainer = styled.ul`
   vertical-align: center;
   width: 100%;
-  margin-top: 2.5rem;
+  margin-top: 8rem;
   padding: 0;
   margin-bottom: 5rem;
 `;
@@ -40,11 +56,13 @@ const IngrdName = styled.p`
   font-weight: bold;
 `;
 
-const SaveBtn = styled.button`
+const Btn = styled.button`
+color:white;
+background: black;
   border-radius: 50%;
-  background: white;
-  width: 50px;
-  height: 50px;
+  padding:10px;
+  width: 70px;
+  height: 70px;
   border: none;
   font-family: "NanumSquare";
   &:active {
@@ -56,27 +74,51 @@ function SAPresenter ({ allergyList, save, onToggle }) {
   return (
     <>
       <Title>
-        Select from <br></br>21 Ingredients Check List
-        <SaveBtn onClick={save}>
-          <b>SAVE</b>
-        </SaveBtn>
+        {/* <SaveBtn onClick={save}>
+          <BiAddToQueue size="30" />
+          <b >Add</b>
+        </SaveBtn> */}
+        <div>
+          Ingredients Check List <SubTitle>Select ingredients you can't eat  </SubTitle>
+        </div>
+
+        {/* <SaveBtn onClick={save}>
+          <GrSave size="30" />
+          <b >Save</b>
+        </SaveBtn> */}
+
       </Title>
 
-      <IngrdContainer>
-        {allergyList.map((el) => {
-          return (
-            <Ingrd
-              isClick={el.checked}
-              key={el.id}
-              onClick={() => onToggle(el.id)}
-            >
-              {/* <IngrdImg src={`${ipObj.ip}/images/${el.image}`} alt="FoodImg" /> */}
-              <IngrdImg src={el.image} alt="FoodImg" />
-              <IngrdName>{el.name}</IngrdName>
-            </Ingrd>
-          );
-        })}
-      </IngrdContainer>
+      < IngrdContainer >
+        {
+          allergyList.map((el) => {
+            return (
+              <Ingrd
+                isClick={el.checked}
+                key={el.id}
+                onClick={() => onToggle(el.id)}
+              >
+                {/* <IngrdImg src={`${ipObj.ip}/images/${el.image}`} alt="FoodImg" /> */}
+                <IngrdImg src={el.image} alt="FoodImg" />
+                <IngrdName>{el.name}</IngrdName>
+              </Ingrd>
+            );
+          })
+        } </IngrdContainer>
+
+      <Footer>
+        <Btn onClick={save}>
+          <BiAddToQueue size="30" />
+          <b >Add</b>
+        </Btn>
+
+
+        <Btn onClick={save}>
+          <GiSaveArrow size="30" />
+          <b >Save</b>
+        </Btn>
+
+      </Footer>
     </>
   );
 }
