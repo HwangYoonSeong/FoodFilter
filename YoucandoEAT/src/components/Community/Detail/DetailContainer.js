2e79f59abe8f34a8a5baffa464b5a11cdfae6c47
gunwoongPark <olympiodoros@naver.com>
2021-05-22 18:02:04 +0900

    redux 문법 변경 5

--- YoucandoEAT/src/components/Community/Detail/DetailContainer.js
+++ YoucandoEAT/src/components/Community/Detail/DetailContainer.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import DetailPresenter from "./DetailPresenter";
 import { useParams } from "react-router-dom";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
-function DetailContainer({ uidState }) {
+function DetailContainer() {
+  const uid = useSelector((state) => state.uid);
   const { pid } = useParams();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
-    console.log(`접속 유저 uid : ${uidState}`);
+    console.log(`접속 유저 uid : ${uid}`);
     console.log(
       `페이지 고유 아이디인 pid : ${pid}를 서버로 보내어 적절한 게시물을 받아오는 통신 수행`
     );
     setPost("해당페이지");
-  }, [pid, uidState]);
+  }, [pid, uid]);
 
   return (
     <>
@@ -22,10 +23,4 @@ function DetailContainer({ uidState }) {
   );
 }
 
-function stateTOprops(state) {
-  return {
-    uidState: state.uidReducer,
-  };
-}
-
-export default connect(stateTOprops)(DetailContainer);
+export default DetailContainer;
