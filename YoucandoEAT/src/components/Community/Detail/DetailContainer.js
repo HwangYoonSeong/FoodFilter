63008e8fb49f32fe66938eeed109cce3c09f0fea
gunwoongPark <olympiodoros@naver.com>
2021-06-05 19:49:36 +0900

    코멘트 더미 세팅

--- YoucandoEAT/src/components/Community/Detail/DetailContainer.js
+++ YoucandoEAT/src/components/Community/Detail/DetailContainer.js
@@ -3,29 +3,77 @@ import DetailPresenter from "./DetailPresenter";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
+import Dummy from "../../../assets/Lenna.png";
+
 import axios from "axios";
 import ipObj from "../../../key";
 
 function DetailContainer() {
   const dispatch = useDispatch();
+
+  const [modal, setModal] = useState(false);
   const { pid } = useParams();
-  const [post, setPost] = useState([]);
+  const [post, setPost] = useState(null);
   const [comment, setComment] = useState("");
 
+  // useEffect(() => {
+  //   axios
+  //     .get(`${ipObj.ip}/postDetail/${pid}`)
+  //     .then((response) => {
+  //       //post, comment, user join해서 한번에 정보를 받음
+  //       setPost(response.data.results);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err.response);
+  //     });
+  // }, [pid, dispatch]);
+
+  // 더미 데이터
   useEffect(() => {
-    axios
-      .get(`${ipObj.ip}/postDetail/${pid}`)
-      .then((response) => {
-        //post, comment, user join해서 한번에 정보를 받음
-        setPost(response.data.results);
-      })
-      .catch((err) => {
-        console.error(err.response);
-      });
-  }, [pid, dispatch]);
+    setPost({
+      userImg: Dummy,
+      writer: "Lenna",
+      date: "2021-06-06 19:43",
+      title: "Dummy Title",
+      postImg: Dummy,
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      comments: [
+        {
+          id: 0,
+          writerImg: Dummy,
+          writer: "Lenna",
+          contents: "short contents",
+          date: "2021-06-06 19:43",
+        },
+
+        {
+          id: 0,
+          writerImg: Dummy,
+          writer: "Lenna",
+          contents: "short contents",
+          date: "2021-06-06 19:43",
+        },
+
+        {
+          id: 0,
+          writerImg: Dummy,
+          writer: "Lenna",
+          contents: "short contents",
+          date: "2021-06-06 19:43",
+        },
+      ],
+    });
+  }, []);
 
   const clickEnter = () => {
     console.log(comment);
+
+    if (comment === "") {
+      setModal(true);
+      return;
+    }
+
     setComment("");
     // axios
     //   .post(`${ipObj.ip}/commentInput`, { "pid": pid, "writerId": uid, "comment": comment }, {
@@ -52,6 +100,8 @@ function DetailContainer() {
         onChangeInputs={onChangeInputs}
         clickEnter={clickEnter}
         comment={comment}
+        modal={modal}
+        setModal={setModal}
       />
     </>
   );
--- YoucandoEAT/src/components/Community/Detail/DetailPresenter.js
+++ YoucandoEAT/src/components/Community/Detail/DetailPresenter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 import ipObj from "../../../key";
 const ContentsContainer = styled.div`
@@ -62,17 +62,123 @@ const Input = styled.textarea`
   }
 `;
 
+const fadeIn = keyframes`
+  from{
+    opacity:0;
+  }
+  to{
+    opacity:1;
+  }
+`;
+
+const DarkBackground = styled.div`
+  ${(props) =>
+    props.modal
+      ? css`
+          display: block;
+        `
+      : css`
+          display: none;
+        `}
+
+  position:fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+`;
+
+const slideUp = keyframes`
+  from{
+    transform: translate(-50%, 0%);
+  }
+  to{
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const ModalBox = styled.div`
+  ${(props) =>
+    props.modal
+      ? css`
+          display: block;
+        `
+      : css`
+          display: none;
+        `};
+
+  position: fixed;
+  width: 325px;
+  height: 175px;
+  background: white;
+  border-radius: 0.5rem;
+  z-index: 10;
+  left: 50%;
+  bottom: 50%;
+  transform: translate(-50%, -50%);
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+`;
+
+const ModalBody = styled.div`
+  border-bottom: 1px dashed black;
+  height: 70%;
+`;
+
+const ModalContents = styled.div`
+  padding: 1rem;
+  font-size: 18px;
+`;
+
+const ModalFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 30%;
+`;
+
+const ModalBtn = styled.button`
+  outline: none;
+  border: none;
+  background: #2b8a3e;
+  color: white;
+  margin-right: 1rem;
+  font-size: 20px;
+  border-radius: 0.25rem;
+`;
+
 function DetailPresenter({
   post,
 
   onChangeInputs,
   clickEnter,
   comment,
+  modal,
+  setModal,
 }) {
   return (
-    <div>
+    <>
+      <DarkBackground modal={modal} />
+      <ModalBox modal={modal}>
+        <ModalBody>
+          <ModalContents>The comment is empty.</ModalContents>
+        </ModalBody>
+        <ModalFooter>
+          <ModalBtn onClick={() => setModal(false)}>Ok</ModalBtn>
+        </ModalFooter>
+      </ModalBox>
+
       {/* CONTENT */}
-      {!Array.isArray(post) ? (
+      {!post ? (
         <ContentsContainer>
           <table>
             <tbody>
@@ -142,7 +248,7 @@ function DetailPresenter({
           }}
         />
       </InputContainer>
-    </div>
+    </>
   );
 }
 
