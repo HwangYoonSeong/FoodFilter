7d493dbd88f71cbb94bac92a5e3e75df12694ad2
gunwoongPark <olympiodoros@naver.com>
2021-06-05 16:40:58 +0900

    커스텀 모달 구현 후 지정

--- YoucandoEAT/src/components/Community/WritePost/WritePostContainer.js
+++ YoucandoEAT/src/components/Community/WritePost/WritePostContainer.js
@@ -1,15 +1,16 @@
 import React, { useState, useCallback } from "react";
-import WritePostPresenter from "./WritePostPresenter"
+import WritePostPresenter from "./WritePostPresenter";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import ipObj from "../../../key"
+import ipObj from "../../../key";
 
-function WritePostContainer ({ history }) {
+function WritePostContainer({ history }) {
+  const [modal, setModal] = useState(false);
   const uid = useSelector((state) => state.uid);
   const [image, setImage] = useState(null);
   const [inputs, setInputs] = useState({
-    title: '',
-    content: ''
+    title: "",
+    content: "",
   });
 
   const onChangeImage = (e) => {
@@ -20,9 +21,9 @@ function WritePostContainer ({ history }) {
     const reader = new FileReader();
     reader.readAsDataURL(files[0]);
     reader.onload = () => {
-      setImage(reader.result)
-    }
-  }
+      setImage(reader.result);
+    };
+  };
 
   const onChangeInputs = useCallback(
     (e) => {
@@ -35,16 +36,25 @@ function WritePostContainer ({ history }) {
     [inputs]
   );
 
-
   const onClick = useCallback(() => {
+    console.log(image);
+    console.log(inputs);
+
+    if (inputs.title === "" || inputs.content === "") {
+      setModal(true);
+    }
     // uid, image, title, content
     // 서버로 보내기
     axios
-      .post(`${ipObj.ip}/postInput`, { "inputs": inputs, "writerId": uid }, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
+      .post(
+        `${ipObj.ip}/postInput`,
+        { inputs: inputs, writerId: uid },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
       .then((response) => {
         console.log(response);
         history.goBack();
@@ -52,18 +62,20 @@ function WritePostContainer ({ history }) {
       .catch((err) => {
         console.error(err.response);
       });
-
-
-    console.log(image);
-    console.log(inputs);
   }, [image, inputs, history, uid]);
 
   return (
     <>
-      <WritePostPresenter onChangeImage={onChangeImage} onClick={onClick} inputs={inputs} onChangeInputs={onChangeInputs} />
+      <WritePostPresenter
+        onChangeImage={onChangeImage}
+        onClick={onClick}
+        inputs={inputs}
+        onChangeInputs={onChangeInputs}
+        modal={modal}
+        setModal={setModal}
+      />
     </>
   );
 }
-
 
 export default React.memo(WritePostContainer);
--- YoucandoEAT/src/components/Community/WritePost/WritePostPresenter.js
+++ YoucandoEAT/src/components/Community/WritePost/WritePostPresenter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 import { FiCamera } from "react-icons/fi";
 import { AiOutlineCheck } from "react-icons/ai";
 
@@ -70,14 +70,120 @@ const CompBtn = styled.button`
   background: #e9ecef;
 `;
 
+const fadeIn = keyframes`
+  from{
+    opacity:0;
+  }
+  to{
+    opacity:1;
+  }
+`;
+
+const DarkBackground = styled.div`
+  ${(props) =>
+    props.modal
+      ? css`
+          display: block;
+        `
+      : css`
+          display: none;
+        `}
+
+  position:fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+`;
+
+const slideUp = keyframes`
+  from{
+    transform: translate(-50%, 0%);
+  }
+  to{
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const ModalBox = styled.div`
+  ${(props) =>
+    props.modal
+      ? css`
+          display: block;
+        `
+      : css`
+          display: none;
+        `};
+
+  position: fixed;
+  width: 325px;
+  height: 175px;
+  background: white;
+  border-radius: 0.5rem;
+  z-index: 10;
+  left: 50%;
+  bottom: 50%;
+  transform: translate(-50%, -50%);
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+`;
+
+const ModalBody = styled.div`
+  border-bottom: 1px dashed black;
+  height: 70%;
+`;
+
+const ModalContents = styled.div`
+  padding: 1rem;
+  font-size: 18px;
+`;
+
+const ModalFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 30%;
+`;
+
+const ModalBtn = styled.button`
+  outline: none;
+  border: none;
+  background: #2b8a3e;
+  color: white;
+  margin-right: 1rem;
+  font-size: 20px;
+  border-radius: 0.25rem;
+`;
+
 function WritePostPresenter({
   onChangeImage,
   onClick,
   onChangeInputs,
   inputs,
+  modal,
+  setModal,
 }) {
   return (
     <>
+      <DarkBackground modal={modal} />
+      <ModalBox modal={modal}>
+        <ModalBody>
+          <ModalContents>The title or content is empty.</ModalContents>
+        </ModalBody>
+        <ModalFooter>
+          <ModalBtn onClick={() => setModal(false)}>Ok</ModalBtn>
+        </ModalFooter>
+      </ModalBox>
+
       <WriteContainer>
         <InputTitleContainer>
           <InputTitle
