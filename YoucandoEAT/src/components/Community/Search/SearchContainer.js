e7d43ca03699840f38c18f9b537d2d4d4b9dac1a
gunwoongPark <olympiodoros@naver.com>
2021-06-06 20:36:08 +0900

    검색 기능 예외처리

--- YoucandoEAT/src/components/Community/Search/SearchContainer.js
+++ YoucandoEAT/src/components/Community/Search/SearchContainer.js
@@ -3,9 +3,9 @@ import SearchPresenter from "./SearchPresenter";
 import { useDispatch } from "react-redux";
 import { setSearchMode } from "../../../modules/searchMode";
 import axios from "axios";
-import ipObj from "../../../key"
+import ipObj from "../../../key";
 
-function SearchContainer () {
+function SearchContainer() {
   const dispatch = useDispatch();
 
   const [input, setInput] = useState("");
@@ -22,20 +22,24 @@ function SearchContainer () {
   };
 
   const clickEnter = useCallback(() => {
-    // 서버로 사용자의 input을 보내어 게시판 데이터를 요청하여
-    // setPosts를 통해 데이터 갱신
+    if (input === "") return;
+
     axios
       .get(`${ipObj.ip}/postSearch?input=${input}`)
       .then((response) => {
-        //post, comment, user join해서 한번에 정보를 받음
-        console.log("url:", `GET postSearch?input=${input}`, "\nstatus:", response.status, "\nstatusText:", response.statusText);
+        console.log(
+          "url:",
+          `GET postSearch?input=${input}`,
+          "\nstatus:",
+          response.status,
+          "\nstatusText:",
+          response.statusText
+        );
         setSearchPosts(response.data.results);
-
       })
       .catch((err) => {
         console.error(err.response);
       });
-
   }, [input]);
 
   const onChange = (e) => {
