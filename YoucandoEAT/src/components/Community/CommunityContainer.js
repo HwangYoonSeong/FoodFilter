2964ed4c811b6b17e5be6aab929efeb2369e229c
gunwoongPark <olympiodoros@naver.com>
2021-06-11 17:23:54 +0900

    커뮤니티 무한 스크롤 구현 완료

--- YoucandoEAT/src/components/Community/CommunityContainer.js
+++ YoucandoEAT/src/components/Community/CommunityContainer.js
@@ -7,30 +7,16 @@ import { setSearchMode } from "../../modules/searchMode";
 
 function CommunityContainer() {
   const uid = useSelector((state) => state.uid);
+  const searchMode = useSelector((state) => state.searchMode);
+
   const dispatch = useDispatch();
 
-  const searchMode = useSelector((state) => state.searchMode);
   const [posts, setPosts] = useState([]);
   const [pageCount, setPageCount] = useState(0);
 
   const openSearch = useCallback(() => {
     dispatch(setSearchMode(true));
   }, [dispatch]);
-
-  const fetchPost = () => {
-    axios
-      .get(`${ipObj.ip}/postList?page=${pageCount}`)
-      .then((response) => {
-        console.log(response);
-        // setPosts(
-        //   posts.concat(posts.concat(response.data.results.slice(0).reverse()))
-        // );
-        // setPosts(response.data.results.slice(0).reverse());
-      })
-      .catch((err) => {
-        console.error(err.response);
-      });
-  };
 
   const infiniteScroll = useCallback(() => {
     let scrollHeight = Math.max(
@@ -48,25 +34,32 @@ function CommunityContainer() {
     }
   }, [pageCount]);
 
-  // 최초의 데이터 요청
-  useEffect(() => {
-    fetchPost();
-  }, []);
-
+  // 스크롤 이벤트 리스너 핸들링
   useEffect(() => {
     window.addEventListener("scroll", infiniteScroll, true);
     return () => window.removeEventListener("scroll", infiniteScroll, true);
   }, [infiniteScroll]);
 
+  // 최초 서버로 요청 및
   // pageCount변수가 증가할 경우(스크롤이 바닥)
   // 서버로 요청
+  useEffect(() => {
+    axios
+      .get(`${ipObj.ip}/postList?page=${pageCount}`)
+      .then((response) => {
+        setPosts(posts.concat(response.data.results));
+      })
+      .catch((err) => {
+        console.error(err.response);
+      });
+  }, [pageCount]);
+
   // 컴포넌트 언마운트 시 검색 모드 비활성화
   useEffect(() => {
-    fetchPost();
     return () => {
       dispatch(setSearchMode(false));
     };
-  }, [pageCount]);
+  }, [dispatch]);
 
   return (
     <>
